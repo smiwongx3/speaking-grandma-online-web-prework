1c01fff7984c1922abb2e1c5acb5ed2aeddeb7d0
Samantha Wong <smiwong920@gmail.com>
2019-01-30 21:31:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,10 @@
 def speak_to_grandma(phrase)
-  if phrase.lowcase 
+  if phrase.downcase 
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase.upcase
+    return "NO, NOT ISNCE 1938!"
+  else phrase = "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!" 
 end
 
 # Whatever you say to grandma, she should respond with
